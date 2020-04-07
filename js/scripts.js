668016b2044ae5ecf4f9d6defd1094e17eac8793
Mikah Mathews <mikah@outofband.org>
2020-04-07 11:53:27 -0700

    Add functionality for multiple email types, addresses

--- js/scripts.js
+++ js/scripts.js
@@ -38,16 +38,21 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 //Business Logic for Contacts
-function Contact (firstName, lastName, phoneNumber) {
+function Contact (firstName, lastName, phoneNumber, emailAddress, addressType, streetName, cityName, stateName, zipCode) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.emailAddress = emailAddress;
+  this.addressType = addressType;
+  this.streetName = streetName;
+  this.cityName = cityName;
+  this.stateName = stateName;
+  this.zipCode = zipCode;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
 
 // User Interface Logic
 var addressBook = new AddressBook();
@@ -67,10 +72,18 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber); 
+    var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedAddressType = $("#new-email-type input[type='radio']:checked").val();
+    var inputtedStreetAddress = $("input#new-street-address").val();
+    var inputtedCity = $("input#city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZipCode = $("input#new-zip-code").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedAddressType, inputtedStreetAddress, inputtedCity, inputtedState, inputtedZipCode); 
     addressBook.addContact(newContact);
+    console.log("This is the Address type " + inputtedAddressType);
     console.log(addressBook.contacts);
     displayContactDetails(addressBook);
+    
     
   });
 });
